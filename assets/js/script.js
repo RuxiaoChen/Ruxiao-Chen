e3b3cdc9361a3f947a586f821d41cb0740ed1a7e
RuxiaoChen <111297841+RuxiaoChen@users.noreply.github.com>
2024-05-06 16:19:29 +0800

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ overlay.addEventListener("click", testimonialsModalFunc);
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
-const filterBtn = document.querySelectorAll("[data-filter-btn]");
+//const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
 select.addEventListener("click", function () { elementToggleFunc(this); });
 
@@ -70,48 +70,48 @@ for (let i = 0; i < selectItems.length; i++) {
     let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
     elementToggleFunc(select);
-    filterFunc(selectedValue);
+//    filterFunc(selectedValue);
 
   });
 }
 
 // filter variables
-const filterItems = document.querySelectorAll("[data-filter-item]");
-
-const filterFunc = function (selectedValue) {
-
-  for (let i = 0; i < filterItems.length; i++) {
-
-    if (selectedValue === "all") {
-      filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
-      filterItems[i].classList.add("active");
-    } else {
-      filterItems[i].classList.remove("active");
-    }
-
-  }
-
-}
-
-// add event in all filter button items for large screen
-let lastClickedBtn = filterBtn[0];
-
-for (let i = 0; i < filterBtn.length; i++) {
-
-  filterBtn[i].addEventListener("click", function () {
-
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    filterFunc(selectedValue);
-
-    lastClickedBtn.classList.remove("active");
-    this.classList.add("active");
-    lastClickedBtn = this;
-
-  });
-
-}
+//const filterItems = document.querySelectorAll("[data-filter-item]");
+//
+//const filterFunc = function (selectedValue) {
+//
+//  for (let i = 0; i < filterItems.length; i++) {
+//
+//    if (selectedValue === "all") {
+//      filterItems[i].classList.add("active");
+//    } else if (selectedValue === filterItems[i].dataset.category) {
+//      filterItems[i].classList.add("active");
+//    } else {
+//      filterItems[i].classList.remove("active");
+//    }
+//
+//  }
+//
+//}
+//
+//// add event in all filter button items for large screen
+//let lastClickedBtn = filterBtn[0];
+//
+//for (let i = 0; i < filterBtn.length; i++) {
+//
+//  filterBtn[i].addEventListener("click", function () {
+//
+//    let selectedValue = this.innerText.toLowerCase();
+//    selectValue.innerText = this.innerText;
+//    filterFunc(selectedValue);
+//
+//    lastClickedBtn.classList.remove("active");
+//    this.classList.add("active");
+//    lastClickedBtn = this;
+//
+//  });
+//
+//}
 
 
 
